99d9d441d6b2ecbaaf495cefd62eaa422000975e
Thais e Laerte <laerte.guimaraes@live.com>
2019-07-16 20:52:13 -0300

    bundled rspec

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,9 +1,30 @@
 class RecipesController < ApplicationController
+  
   def index
     @recipes = Recipe.all
   end
 
   def show
     @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    # @recipe.save
+    redirect_to recipe_path(@recipe)
+  end
+
+  def edit
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :recipe_type, 
+    :cuisine, :difficulty, :cook_time, :ingredients, :cook_method)
   end
 end
