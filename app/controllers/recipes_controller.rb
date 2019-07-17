c028d6032fb4cd3364c36bdaa939b3c1461ee2b1
Thais e Laerte <laerte.guimaraes@live.com>
2019-07-16 21:04:54 -0300

    editing recipe - tests passing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,11 +14,17 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.create(recipe_params)
-    # @recipe.save
-    redirect_to recipe_path(@recipe)
+    redirect_to @recipe
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to @recipe
   end
 
   private
