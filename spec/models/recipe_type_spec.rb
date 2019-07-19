bb49d49b63a17d297a6a71a08edcf969a474ce39
Thais e Laerte <laerte.guimaraes@live.com>
2019-07-18 22:06:42 -0300

    added model test to recipe type

--- spec/models/recipe_type_spec.rb
+++ spec/models/recipe_type_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe RecipeType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "should not be valid" do
+    recipe_type = RecipeType.new(name: '')
+    expect(recipe_type).to_not be_valid
+  end
+
+  it "should be valid" do
+    recipe_type = RecipeType.new(name: 'Brasileira')
+    expect(recipe_type).to be_valid
+  end
+
+  it 'should be unique' do
+    recipe_type = RecipeType.create(name: 'Brasileira')
+    recipe_type_new = RecipeType.new(name: 'Brasileira')
+    expect(recipe_type_new).to_not be_valid
+  end
+
+  it '' do
+  end
 end
